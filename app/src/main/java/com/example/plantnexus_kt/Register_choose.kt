43ae1eb8b42edcb77c7684ce04dd49225f5f1744
Sanjeevan <it21334542@my.sliit.lk>
2023-10-20 09:42:46 +0530

    created products

---
+++ app/src/main/java/com/example/plantnexus_kt/Register_choose.kt
@@ -0,0 +1,11 @@
+package com.example.plantnexus_kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_choose : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_choose)
+    }
+}
